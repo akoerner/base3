08f0de212b8734d5c49f178e5f73fe73ae248481
Andrew Koerner <andrew.koerner@dlr.de>
2023-08-30 15:00:32 +0200

    added subscripts

--- base3.bash
+++ base3.bash
@@ -1,4 +1,4 @@
 for (( i=0; i<=729; i++ )); do
     number_in_base3=$(echo "obase=3; $i" | bc)
-    printf "%3d: %9s\n" "$i" "$number_in_base3"
+    printf "(%3d)₁₀: (%7s)₃\n" "$i" "$number_in_base3"
 done
